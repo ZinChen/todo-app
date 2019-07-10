68f5f8fe1ef619d7e2a7735417864bac2e86ee59
Yuri Zinchenko <zinchen777@gmail.com>
2019-07-10 12:51:40 +0300

    add pug and sass, create first screen

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,129 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+#app
+  .main-c
+    .header-c
+      .date-toggle-c
+        .prev-date-c
+          .date-val 26
+          .month-val May
+        .prev-arrow.icon
+          i.ion-chevron-left
+        .current-date
+          .current-date-val Today
+        .next-arrow.icon
+          i.ion-chevron-right
+        .next-date-c
+          .date-val 28
+          .month-val May
+    .body-c
+      .task-c
+        todo-item(
+          itemId=1,
+          title="Develop TODO App",
+          description="Create first page"
+          :scheduled="true"
+        )
+
+        todo-item(
+          itemId=1,
+          title="Daily excercise",
+          description="Do gym"
+        )
+
+      .create-task-c
+        .create-task.task
+          .create-task-icon.icon.icon-is-medium
+            i.ion-plus-round
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItem from './components/TodoItem.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoItem,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+  .task {
+    position: relative;
+    display: flex;
+    align-items: center;
+    height: 55px;
+    margin-bottom: 10px;
+    padding: 10px 5px;
+    border: 1px solid #00B2FE;
+    border-radius: 10px;
+    line-height: 1.2;
+  }
+</style>
+
+
+<style lang="scss" scoped>
+.main-c {
+  width: 320px;
+  margin: 20px auto;
+  padding: 20px;
+  padding-top: 10px;
+  padding-bottom: 30px;
+  border: 1px solid #00B2FE;
+  border-radius: 20px;
+}
+
+.header-c {
+  margin-bottom: 20px;
+}
+
+.date-toggle-c {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.date-val {
+  font-size: 16px;
+  line-height: 1;
+}
+
+.month-val {
+  font-size: 10px;
+  line-height: 1;
+}
+
+.prev-arrow,
+.next-arrow {
+  padding: 0 15px;
+}
+
+.current-date {
+  // text-transform: uppercase;
+}
+
+.body-c {
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
+}
+
+.task-c {
+  flex: 1;
+}
+
+.create-task {
+  display: flex;
+  // justify-content: flex-end;
+  justify-content: center;
+}
+
+.create-task-icon {
+  // margin-right: 10px;
+  line-height: 1;
+  color: #fff;
+  background: #00B2FE;
+  // border: 1px solid #00B2FE;
+  border-radius: 50%;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,61 @@
+<template lang="pug">
+  .task
+    .task-icon.icon
+      i.ion-calendar(v-if="scheduled")
+      i.ion-android-time(v-else)
+    .task-checkbox
+      input(
+        :id="idAttr",
+        :name="itemId"
+        type="checkbox",
+      )
+    .task-content
+      .task-title {{ title }}
+      .task-description {{ description }}
+</template>
+
+
+<script>
+export default {
+  name: 'todo-item',
+  props: {
+    itemId: [String, Number],
+    title: String,
+    description: String,
+    scheduled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    idAttr() {
+      return `item-${this.itemId}`
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+  .task-checkbox {
+    margin-left: 5px;
+    margin-right: 5px;
+  }
+
+  .task-icon {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: .5rem;
+    height: .5rem;
+  }
+
+  .task-title {
+    font-weight: 500;
+    font-size: 16px;
+  }
+
+  .task-description {
+    font-size: 12px;
+  }
+</style>
